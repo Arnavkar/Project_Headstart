8e83898f4dc5348c50335ac6f982921c744c7222
dxsyntax <bboifatt@gmail.com>
2020-10-03 08:19:28 +0800

    implemented plant design for homepage

--- src/components/Homedisplay.vue
+++ src/components/Homedisplay.vue
@@ -3,7 +3,28 @@
         <div class="q-px-xl q-pt-md text-h3 text-dark text-weight-medium">Hi {{this.name}}</div>
         <div class="q-px-xl q-pt-sm text-subtitle2 text-dark text-weight-light">Exclusive Member Since {{this.signUpDate}} </div>
         <div class="q-px-xl q-pt-sm text-subtitle2 text-dark text-weight-light">Your sapling can be nourished when you scan more sustainable products</div>
-        <img src="../assets/Stage1.png" class="plant_img q-pt-md"/>
+
+        <div class="image-container" style=" height: 410px;">
+          
+
+        
+
+        <img src="../assets/Stage1.png" class="plant_img q-pt-md" v-if="points == 1"/>
+
+        <img src="../assets/Stage2.png" class="plant_img q-pt-md" v-if="points == 2" style="top: 118px;"/>
+
+        <img src="../assets/Stage3.png" class="plant_img q-pt-md" v-if="points == 3" style="top: 37px; "/>
+
+        </div>
+
+        <select id="points-quantity" v-model.number="points">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select>
+
+        
+
     </div>
 </template>
 
@@ -17,7 +38,8 @@ export default {
     return{
       name: JSON.parse(localStorage.getItem("userInfo")).Name,
       email: JSON.parse(localStorage.getItem("userInfo")).Email,
-      signUpDate: JSON.parse(localStorage.getItem("userInfo")).signUpDate
+      signUpDate: JSON.parse(localStorage.getItem("userInfo")).signUpDate,
+      points : 1
     }
   }
 }
@@ -32,6 +54,14 @@ export default {
   height: auto;
   width: 60vw;
   border-radius:30px;
+  position: relative;
+  top: 170px;
+}
+
+#points-quantity {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 </style>
